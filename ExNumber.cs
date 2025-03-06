b2cabd0899e93a9b9b4a82b9fc140ddd64ee51ba
Francesco <fra96943@gmail.com>
2025-03-06 21:50:47 +0100

    Add files via upload

---
+++ ExNumber.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C_EX
+{
+    class ExNumber
+    {
+        public string ToRomain(int num)
+        {
+            if (num <= 0)
+            {
+                return string.Empty;
+            }
+
+            if (num < 1 || num > 99999)
+            {
+                throw new ArgumentOutOfRangeException("number", "the number must be between 1 and 3999.");
+            }
+            string[] simboli = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] valori = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+            StringBuilder romano = new StringBuilder();
+
+            for (int i = 0; i < simboli.Length; i++)
+            {
+                while (num >= valori[i])
+                {
+                    num -= valori[i];
+                    romano.Append(simboli[i]); 
+                }
+            }
+
+            return romano.ToString();
+        }
+
+        public bool IsPrime(int num)
+        {
+            if (num <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++) 
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        public bool IsEven(int num)
+        {
+            try
+            {
+                bool v = (num % 2 == 0);
+                return v;
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+                throw;
+            }
+        }
+    }
+}
---
+++ ExString.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_EX
+{
+    class ExString
+    {
+        public static  string ToSentenceCase(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            char[] limiters = { '.', '!', '?' };
+            string[] sentences = value.Split(limiters, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                string sentence = sentences[i].TrimStart();
+                if (!string.IsNullOrEmpty(sentence))
+                {
+                    sentences[i] = char.ToUpper(sentence[0])+(sentence.Length > 1 ? sentence.Substring(1).ToLower() : string.Empty);
+                }
+            }
+            StringBuilder result = new StringBuilder();
+            int currentPos = 0;
+
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                result.Append(sentences[i]);
+                currentPos += sentences[i].Length;
+                if (currentPos < value.Length)
+                {
+                    int nextDelimiterPos = value.IndexOfAny(limiters, currentPos);
+                    if (nextDelimiterPos >= 0 && nextDelimiterPos < value.Length)
+                    {
+                        result.Append(value[nextDelimiterPos]);
+                        currentPos = nextDelimiterPos + 1;
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public string RemoveAccents(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            string normalized = value.Normalize(NormalizationForm.FormD);
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in normalized)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public string GenPassword(int value)
+        {
+            if(value <= 0)
+            {
+                return string.Empty;
+            }
+
+            const string c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+            Random random = new Random();
+            char[] password = new char[value];
+
+            for (int i = 0; i < value; i++)
+            {
+                password[i] = c[random.Next(c.Length)];
+            }
+
+            return new string(password);
+        }
+        public string RemoveCharactersNonAlphanumeric(string input)
+        {
+            return new string(input.Where(c => char.IsLetterOrDigit(c)).ToArray());
+        }
+
+    }
+}
